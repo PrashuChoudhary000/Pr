0b49b3186c3152bfea969f93696032873a46b907
Prashu Chaudhary <chaudharyprashu24@gmail.com>
2025-12-30 10:35:32 +0530

    Connect to Railway backend

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import TaglineSection from "./TaglineSection";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://web-production-6b4b6.up.railway.app/",
 });
 
 function App() {
